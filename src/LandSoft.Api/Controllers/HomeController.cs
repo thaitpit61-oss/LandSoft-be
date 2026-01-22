386ab37c2642f64b9535e38f879667bda1303fe9
BCONS\thaitp <thaitpit.61@gmail.com>
2026-01-22 11:21:21 +0700

    set up db authen

---
+++ src/LandSoft.Api/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LandSoft.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
---
+++ src/LandSoft.Data/LandSoftContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandSoft.Data
+{
+    public class LandSoftContextFactory : IDesignTimeDbContextFactory<LandSoftContext>
+    {
+        public LandSoftContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<LandSoftContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new LandSoftContext(builder.Options);
+        }
+    }
+}
